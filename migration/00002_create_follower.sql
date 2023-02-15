3c89c6fc62d46002c851bd60ba541e0e9c6edaa2
builamquangngoc91 <builamquangngoc91@gmail.com>
2023-02-15 10:53:56 +0700

    add code create table follower

--- migration/00002_create_follower.sql
+++ migration/00002_create_follower.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS follower (
     id TEXT PRIMARY KEY,
-    user_id TEXT NOT NULL,
+    account_id TEXT NOT NULL,
     follower_id TEXT NOT NULL,
     followed_date TIMESTAMPTZ DEFAULT NOW(),
     unfollowed_date TIMESTAMPTZ DEFAULT NOW(),
     created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
+    updated_at TIMESTAMPTZ DEFAULT NOW(),
+    FOREIGN KEY (account_id) REFERENCES account(account_id),
+    FOREIGN KEY (follower_id) REFERENCES account(account_id)
 );
